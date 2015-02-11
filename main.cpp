2990c5a01eef37315bf13781a824dbacee854649
Brian Jackson <axnjaxn@axnjaxn.com>
2015-02-11 16:09:25 -0500

    Added drawing tool and streamlined existing tools.

--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@ protected:
   } rendermode;
 
   int rate;
-  bool emitting;
-  int mx, my;
+  bool emitting, drawing;
+  int mx, my, nx, ny;
 
   void mapPressureColor(double v, ByteImage::BYTE& r, ByteImage::BYTE& g, ByteImage::BYTE& b) {
     r = g = b = 0;
@@ -44,12 +44,23 @@ protected:
 
   void handleEvent(SDL_Event event) {
     if (event.type == SDL_MOUSEBUTTONDOWN) {
-      emitting = 1;
-      mx = event.button.x / sc;
-      my = event.button.y / sc;
+      if (event.button.button == SDL_BUTTON_LEFT)
+	emitting = 1;
+      else if (event.button.button == SDL_BUTTON_RIGHT)
+	drawing = 1;
+      mx = nx = event.button.x / sc;
+      my = ny = event.button.y / sc;
     }
     else if (event.type == SDL_MOUSEBUTTONUP) {
-      emitting = 0;
+      if (event.button.button == SDL_BUTTON_LEFT)
+	emitting = 0;
+      else if (event.button.button == SDL_BUTTON_RIGHT) {
+	drawing = 0;
+	Matrix d = makePoint(mx - nx, my - ny, 0);
+	int n = length(d);
+	for (int i = 0; i <= n; i++)
+	  sim.setWall((int)(ny + d.at(1) * i / n), (int)(nx + d.at(0) * i / n));
+      }
     }
     else if (event.type == SDL_MOUSEMOTION) {
       mx = event.motion.x / sc;
@@ -57,47 +68,24 @@ protected:
     }
     else if (event.type == SDL_KEYDOWN) {
       switch (event.key.keysym.sym) {
-      case SDLK_0:
-	rate = 0;
-	break;
-      case SDLK_1:
-	rate = 1;
-	break;
-      case SDLK_2:
-	rate = 2;
-	break;
-      case SDLK_3:
-	rate = 4;
-	break;
-      case SDLK_4:
-	rate = 8;
-	break;
-      case SDLK_5:
-	rate = 16;
-	break;
-      case SDLK_p:
-	rendermode = PRESSURE;
-	break;
-      case SDLK_e:
-	emitmode = EMIT;
-	break;
-      case SDLK_a:
-	emitmode = ACCEL;
-	break;
-      case SDLK_w:
-	emitmode = WALL;
-	break;
-      case SDLK_c:
-	rendermode = CURL;
-	break;
-      case SDLK_s:
-	rendermode = SPEED;
-	break;
+      case SDLK_0: rate = 0; break;
+      case SDLK_1: rate = 1; break;
+      case SDLK_2: rate = 2; break;
+      case SDLK_3: rate = 4; break;
+      case SDLK_4: rate = 6; break;
+      case SDLK_5: rate = 8; break;
+      case SDLK_p: rendermode = PRESSURE; break;
+      case SDLK_e: emitmode = EMIT; break;
+      case SDLK_a: emitmode = ACCEL; break;
+      case SDLK_w: emitmode = WALL; break;
+      case SDLK_c: rendermode = CURL; break;
+      case SDLK_s: rendermode = SPEED; break;
       case SDLK_o:
 	printf("Set omega (current value: %.2lf)\n", sim.omega);
 	scanf("%lf", &sim.omega);
 	break;
       case SDLK_SPACE:
+	rate = 0;
 	sim.step();
 	break;
       case SDLK_BACKSPACE:
@@ -136,29 +124,28 @@ protected:
 	if (sim.isWall(r, c)) R = G = B = 255;
 	DrawRect(canvas, c * sc, r * sc, sc, sc, R, G, B);
       }
+    if (drawing)
+      DrawLine(canvas, makePoint(nx + 0.5, ny + 0.5, 1.0 / sc), makePoint(mx + 0.5, my + 0.5, 1.0 / sc), makeColor(255, 255, 255));
     updateImage(canvas);
   }
 
   void emit() {
     if (!emitting) return;
 
-    if (emitmode == EMIT) {
-      for (int i = -radius; i <= radius; i++)
-	for (int j = -radius; j <= radius; j++)
-	  if (i * i + j * j <= radius * radius)
+    for (int i = -radius; i <= radius; i++)
+      for (int j = -radius; j <= radius; j++)
+	if (i * i + j * j <= radius * radius)
+	  switch (emitmode) {
+	  case EMIT:
 	    sim.emitAt(my + i, mx + j);
-    }
-    else if (emitmode == ACCEL) {
-      for (int i = -radius; i <= radius; i++)
-	for (int j = -radius; j <= radius; j++)
-	  if (i * i + j * j <= radius * radius)
+	    break;
+	  case ACCEL:
 	    sim.accelAt(my + i, mx + j);
-    }
-    else if (emitmode == WALL) {
-      for (int i = -radius; i <= radius; i++)
-	for (int j = -radius; j <= radius; j++)
-	  sim.setWall(my + i, mx + j);
-    }
+	    break;
+	  case WALL:
+	    sim.setWall(my + i, mx + j);
+	    break;
+	  }
   }
 
   void update() {
@@ -180,7 +167,7 @@ public:
 
     emitmode = EMIT;
     rendermode = PRESSURE;
-    emitting = 0;
+    emitting = drawing = 0;
     radius = 3;
     rate = 1;
   }
